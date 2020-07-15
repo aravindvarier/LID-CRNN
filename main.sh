4e8f5ba162e46526feea03cb7e19ebaf08525c2a
aravindvarier <avarier97@gmail.com>
2020-07-15 17:50:33 -0400

    main shell script added

---
+++ main.sh
@@ -0,0 +1,17 @@
+rm -rf *.csv
+
+rm -rf audio_data_np
+
+rm -rf zipped_index_*
+
+if [ $# -le 1 ]; then
+    ./download-data.sh 15000 english french spanish german italian
+else
+    ./download-data.sh $1 english french spanish german italian
+fi
+
+python csv_file_creator.py
+
+python train.py
+
+python test.py
